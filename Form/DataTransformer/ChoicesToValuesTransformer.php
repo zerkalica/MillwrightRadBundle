52498738ff1c07bad5b0e0fafbb7437c7b1d1e01
Sergey Yuferev <s.yuferev@boombate.com>
2012-08-07 20:20:37 +0400

    updated form type

---
+++ Form/DataTransformer/ChoicesToValuesTransformer.php
@@ -0,0 +1,40 @@
+<?php
+namespace Millwright\RadBundle\Form\DataTransformer;
+
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\UnexpectedTypeException;
+use Symfony\Component\Form\Extension\Core\ChoiceList\ChoiceListInterface;
+use Symfony\Component\Form\Util\PropertyPath;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+class ChoicesToValuesTransformer implements DataTransformerInterface
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @param ArrayCollection $array array collection of model objects
+     *
+     * @return array of model objects
+     *
+     * @throws UnexpectedTypeException if the given value is not an array
+     */
+    public function transform($array)
+    {
+        return iterator_to_array($array);
+    }
+
+    /**
+     * Do nothing reverse transform
+     *
+     * @param array $array
+     *
+     * @return array
+     */
+    public function reverseTransform($array)
+    {
+        return $array;
+    }
+}
--- Form/Type/EntitySelectFormType.php
+++ Form/Type/EntitySelectFormType.php
@@ -1,50 +1,126 @@
 <?php
 namespace Millwright\RadBundle\Form\Type;
 
-use Millwright\RadBundle\Form\AbstractType;
 use Millwright\RadBundle\Form\DataTransformer\ModelToDataTransformer;
 
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\Options;
 
+use Millwright\RadBundle\Form\AbstractType;
+
+use Symfony\Component\Form\Extension\Core\ChoiceList\ObjectChoiceList;
+
+use Millwright\RadBundle\Form\DataTransformer\ChoicesToValuesTransformer;
+
+use Symfony\Component\Form\Util\PropertyPath;
 /**
  * GroupSelect form type
  */
 class EntitySelectFormType extends AbstractType
 {
     /**
+     * Caches created choice lists.
+     * @var array
+     */
+    private $choiceListCache = array();
+
+    /**
      * {@inheritDoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $transformer = new ModelToDataTransformer($options['collection'], $options['labelPath'], $options['valuePath']);
-
-        $builder->resetViewTransformers();
-        $builder->addViewTransformer($transformer);
+        $builder->addViewTransformer(new ChoicesToValuesTransformer, true);
     }
 
     /**
-     * @return string
+     * {@inheritdoc}
      */
-    public function getParent()
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        return 'choice';
+        $choiceListCache =& $this->choiceListCache;
+
+        $choiceList = function (Options $options) use (&$choiceListCache) {
+            // Harden against NULL values (like in EntityType and ModelType)
+            $choices = null !== $options['choices'] ? $options['choices'] : array();
+
+            if (is_object($choices)) {
+                $choicesKeys = spl_object_hash($choices);
+            } else {
+                $choicesKeys = array_map(function($choice) {
+                    return $choice->getId();
+                } , $choices);
+            }
+
+            // Reuse existing choice lists in order to increase performance
+            $hash = md5(json_encode(array($choicesKeys, $options['preferred_choices'])));
+
+            if (!isset($choiceListCache[$hash])) {
+                $choiceListCache[$hash] = new ObjectChoiceList($choices, $options['labelPath'], $options['preferred_choices'], null, $options['valuePath']);
+            }
+
+            return $choiceListCache[$hash];
+        };
+
+        $emptyData = function (Options $options) {
+            if ($options['multiple'] || $options['expanded']) {
+                return array();
+            }
+
+            return '';
+        };
+
+        $emptyValue = function (Options $options) {
+            return $options['required'] ? null : '';
+        };
+
+        $emptyValueNormalizer = function (Options $options, $emptyValue) {
+            if ($options['multiple'] || $options['expanded']) {
+                // never use an empty value for these cases
+                return null;
+            } elseif (false === $emptyValue) {
+                // an empty value should be added but the user decided otherwise
+                return null;
+            }
+
+            // empty value has been set explicitly
+            return $emptyValue;
+        };
+
+        $compound = function (Options $options) {
+            return $options['expanded'];
+        };
+
+        $resolver->setDefaults(array(
+                'multiple'          => false,
+                'expanded'          => false,
+                'choice_list'       => $choiceList,
+                'choices'           => array(),
+                'preferred_choices' => array(),
+                'empty_data'        => $emptyData,
+                'empty_value'       => $emptyValue,
+                'error_bubbling'    => false,
+                'compound'          => $compound,
+
+                'labelPath'          => 'name',
+                'valuePath'          => 'id',
+            ));
+
+        $resolver->setNormalizers(array(
+                'empty_value' => $emptyValueNormalizer,
+            ));
+
+        $resolver->setAllowedTypes(array(
+                'choice_list' => array('null', 'Symfony\Component\Form\Extension\Core\ChoiceList\ChoiceListInterface'),
+            ));
     }
 
     /**
      * {@inheritDoc}
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    public function getParent()
     {
-        parent::setDefaultOptions($resolver);
-
-        $resolver->setDefaults(array(
-            'show_child_legend'  => false,
-            'translation_domain' => null,
-            'collection'         => array(),
-            'labelPath'          => 'name',
-            'valuePath'          => 'id'
-        ));
+        return 'choice';
     }
 
     /**
